87cd352937962f0ce236b4728088607808f947f3
Samuel Phillips <sam@samuelphillips.net>
2018-03-24 02:10:30 -0400

    Initial commit

---
+++ zippyshare-download-noscript.user.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name        ZippyShare Noscript Download
+// @namespace   samuelphillips.net
+// @description Download files from ZippyShare without enabling scripts
+// @include     http*://*.zippyshare.com/v/*/file.html
+// @version     1
+// @grant       none
+// @run-at      document-end
+// ==/UserScript==
+
+/*
+ * COPYRIGHT 2017 Samuel Phillips
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+
+    if (document.getElementById('fimage')) {
+        document.getElementById('fimage').href = "/i/0EwNJF4b/"+(Math.pow(a, 3)+b)+"/The%20Adventures%20of%20Tintin%20-%20Cigars%20of%20the%20Pharaoh%20%281934%29%20%282048px%29%20%28Minutemen-Syl3ntBob%29.cbz";
+    }
+
+
+*/
+
+(function() {
+    var script = document.querySelector('#lrbox > div:nth-child(1) ' +
+        '> div:nth-child(2) > div:nth-child(1) > script:nth-child(2)');
+    var m1 = /^\s*var\s+a\s*=\s*(\d+)\s*;/.exec(script.innerText);
+    var m2 = /document\.getElementById\('dlbutton'\)\.href\s*=\s*"([^"]*)"\+\(Math\.pow\(a,\s*(\d+)\)\+b\)\+"([^"]*)"/.exec(script.innerText);
+    if (!m1 || !m2) {
+        console.log("Could not extract URL");
+        return;
+    }
+    var a = parseInt(m1[1]);
+    var t1 = m2[1];
+    var apow = parseInt(m2[2]);
+    var t2 = m2[3];
+    location.href = t1 + String(Math.pow(a, apow) + 3) + t2;
+})();
